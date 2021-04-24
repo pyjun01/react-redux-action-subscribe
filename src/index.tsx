927a56e266a097bf7a0adbf600c1a9e76e2094be
pyjun01 <pyjun02@gmail.com>
2021-04-24 12:12:44 +0900

    create store

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,44 @@
 import { useEffect } from 'react';
 import { Middleware } from 'redux';
 
+const Store: {
+  listeners: Handler[];
+  subscribe: (obj: Handler) => () => void;
+} = {
+  listeners: [],
+  subscribe(obj: Handler) {
+    this.listeners.push(obj);
+
+    return () => {
+      const index = this.listeners.indexOf(obj);
+
+      this.listeners.splice(index, 1);
+    };
+  },
+}
+
 interface Handler {
   [key: string]: (payload: unknown) => void;
 }
 
 export const useActionCallback = (sub: Handler) => {
   useEffect(() => {
-    
+    const unsubscribe = Store.subscribe(sub);
+
+    return unsubscribe;
   }, []);
 }
 
-const ActionMiddleware: Middleware = () => (next) => (action) => {
-  const { type } = action;
+const actionCallback: Middleware = () => (next) => (action) => {
+  const { type, payload } = action;
 
   if (type) {
-
+    for (let i = 0; i < Store.listeners.length; i++) {
+      Store.listeners[i] && Store.listeners[i][type] && Store.listeners[i][type](payload);
+    }
   }
 
   next(action);
 }
 
-export default ActionMiddleware;
+export default actionCallback;
